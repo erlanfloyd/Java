504910c376091ba83efbad8cb388d10d8b6d18d3
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-13 17:51:40 +0600

    Add files via upload

---
+++ Tutorials/src/arrays.java
@@ -0,0 +1,14 @@
+public class arrays {
+    public static void main(String[] args) {
+        String name = "David";
+        int age = 42;
+        double score = 15.9;
+        char group = 'Z'; {
+            System.out.println(name);
+            System.out.println(age);
+            System.out.println(score);
+            System.out.println(group);
+        }
+    }
+
+}
