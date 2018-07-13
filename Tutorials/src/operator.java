cf4812a323f154a97b7621311905256984e33a61
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-13 18:31:42 +0600

    Add files via upload

---
+++ Tutorials/src/operator.java
@@ -0,0 +1,9 @@
+public class operator {
+    public static void main(String[] args) {
+        int num1 = 4;
+        int num2 = 8;
+        num2 += num1;
+
+        System.out.println(num2);
+    }
+}
