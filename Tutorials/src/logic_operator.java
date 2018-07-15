17b2e791db5041269d429428eae316199925acdd
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-15 17:40:34 +0600

    Add files via upload

---
+++ Tutorials/src/logic_operator.java
@@ -0,0 +1,12 @@
+public class logic_operator {
+    public static void main(String[] args) {
+        int age = 22;
+        int money = 800;
+
+        if (age > 18) {
+            if (money > 500) {
+                System.out.println("Welcome");
+            }
+        }
+    }
+}
