d847a4d795243e25830a7ebd73f2d63dd54907a1
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-15 17:38:53 +0600

    Add files via upload

---
+++ Tutorials/src/operator_or.java
@@ -0,0 +1,10 @@
+public class operator_or {
+    public static void main(String[] args) {
+        int age = 25;
+        int money = 100;
+
+        if (age > 18 || money > 500) {
+            System.out.println("Welcome!");
+        }
+    }
+}
