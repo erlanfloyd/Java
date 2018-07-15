ed5ef8680e98608dce7642fae2d15b07acc49937
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-15 17:41:13 +0600

    Add files via upload

---
+++ Tutorials/src/operator_if_else_inside.java
@@ -0,0 +1,14 @@
+public class operator_if_else_inside {
+    public static void main(String[] args) {
+        int age = 25;
+        if(age > 0) {
+            if(age > 16) {
+                System.out.println("Welcome");
+            } else {
+                System.out.println("Too Young");
+            }
+        } else {
+            System.out.println("Error");
+        }
+    }
+}
