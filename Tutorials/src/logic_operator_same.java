9b3c363b9eb52334dc3f1f0ace1da700b865c89a
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-15 17:40:12 +0600

    Add files via upload

---
+++ Tutorials/src/logic_operator_same.java
@@ -0,0 +1,10 @@
+public class logic_operator_same {
+    public static void main(String[] args) {
+        int age = 22;
+        int money = 800;
+
+        if (age > 18 && money > 500) {
+            System.out.println("Welcome");
+        }
+    }
+}
