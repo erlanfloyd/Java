f7a82ca299083326bc05d44efac88e6472fd67c8
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-15 17:41:31 +0600

    Add files via upload

---
+++ Tutorials/src/operator_if_else2.java
@@ -0,0 +1,11 @@
+public class operator_if_else2 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        if (x > y) {
+            System.out.println(x);
+        } else {
+            System.out.println(y);
+        }
+    }
+}
