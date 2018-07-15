f4ab9e343f37b164e8c9c494a72c25e8d05b9789
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-15 17:38:32 +0600

    Add files via upload

---
+++ Tutorials/src/operator_or2.java
@@ -0,0 +1,10 @@
+public class operator_or2 {
+    public static void main(String[] args) {
+        int age = 25;
+        if(!(age > 18)) {
+            System.out.println("Too Young");
+        } else {
+            System.out.println("Welcome");
+        }
+    }
+}
