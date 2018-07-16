b6ca580d5529fc26563976c6f3abfe49f56948f3
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-16 21:59:08 +0600

    Add files via upload

---
+++ Tutorials/src/switch_operator2.java
@@ -0,0 +1,13 @@
+public class switch_operator2 {
+    public static void main(String[] args) {
+        int x = 10;
+        switch (x) {
+            case 10:
+                System.out.println("A");
+                break;
+            case 20:
+                System.out.println("B");
+                break;
+        }
+    }
+}
