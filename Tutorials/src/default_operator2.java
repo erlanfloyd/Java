20aba518889f450f72d8288da4e719bde1498d0c
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-16 21:58:16 +0600

    Add files via upload

---
+++ Tutorials/src/default_operator2.java
@@ -0,0 +1,15 @@
+public class default_operator2 {
+    public static void main(String[] args) {
+        int a = 11;
+        int b = 12;
+        int c = 40;
+
+        switch (a) {
+            case 40:
+            System.out.println(b);
+            break;
+        default:
+            System.out.println(c);
+        }
+    }
+}
