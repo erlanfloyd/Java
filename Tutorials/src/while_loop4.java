9580bd461b31e557b6d75f012844e93ab4299121
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-16 21:56:45 +0600

    Add files via upload

---
+++ Tutorials/src/while_loop4.java
@@ -0,0 +1,9 @@
+public class while_loop4 {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 5;
+        while (x < y) {
+            System.out.println("Forza Italia");
+        }
+    }
+}
