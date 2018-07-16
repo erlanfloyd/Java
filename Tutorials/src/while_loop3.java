1577947015141c1e9f41abf28f4561aa67d7dadd
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-16 21:57:09 +0600

    Add files via upload

---
+++ Tutorials/src/while_loop3.java
@@ -0,0 +1,10 @@
+public class while_loop3 {
+    public static void main(String[] args) {
+        int x = 6;
+        while (x < 10) {
+            System.out.println(x);
+            x++;
+        }
+        System.out.println("Loop ended");
+    }
+}
