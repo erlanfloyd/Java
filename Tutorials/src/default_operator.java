af94ff359ae9f11dfb4738c105771b27fd4c2675
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-16 21:58:47 +0600

    Add files via upload

---
+++ Tutorials/src/default_operator.java
@@ -0,0 +1,16 @@
+public class default_operator {
+    public static void main(String[] args) {
+        int day = 3;
+
+        switch (day) {
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Weekday");
+        }
+    }
+}
