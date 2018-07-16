4271d0fbf214b515571339b4b4387cf21a7b86c6
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-16 21:57:29 +0600

    Add files via upload

---
+++ Tutorials/src/while_loop2.java
@@ -0,0 +1,9 @@
+public class while_loop2 {
+    public static void main(String[] args) {
+        int x = 12;
+        while (x < 100) {
+            System.out.println("Java is cool!");
+            x++;
+        }
+    }
+}
