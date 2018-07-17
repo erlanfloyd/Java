12702ee90d4bd8d52ccf8e4c1fd5794573b0b48d
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-17 16:01:20 +0600

    Add files via upload

---
+++ Tutorials/src/do_while_loop2.java
@@ -0,0 +1,9 @@
+public class do_while_loop2 {
+    public static void main(String[] args) {
+        int x = 1;
+        do {
+            System.out.println(x);
+            x++;
+        } while (x < 0);
+    }
+}
