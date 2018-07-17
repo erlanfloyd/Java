1fb47941dbabbea4c42350735f35300f79593e34
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-17 16:01:41 +0600

    Add files via upload

---
+++ Tutorials/src/do_while_loop.java
@@ -0,0 +1,9 @@
+public class do_while_loop {
+    public static void main(String[] args) {
+        int x = 1;
+        do {
+            System.out.println(x);
+            x++;
+        } while (x < 5);
+    }
+}
