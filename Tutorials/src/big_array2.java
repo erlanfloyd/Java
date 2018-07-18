f68ee53ae3186a7ea8c1a842e88313ddc7a51f6e
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-18 15:36:42 +0600

    Add files via upload

---
+++ Tutorials/src/big_array2.java
@@ -0,0 +1,6 @@
+public class big_array2 {
+    public static void main(String[] args) {
+        String array[][] = { {"Hey", "John", "Bye"}, {"John", "Johnson", "Hello"} };
+        System.out.println(array[1][1]);
+    }
+}
