aa833e97979c9addec0448ba21b62b048cca421c
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-18 15:37:46 +0600

    Add files via upload

---
+++ Tutorials/src/array3.java
@@ -0,0 +1,10 @@
+public class array3 {
+    public static void main(String[] args) {
+        int [] myArr = {6, 42, 3, 7};
+        int sum = 0;
+        for (int x = 0; x < myArr.length; x++) {
+            sum += myArr[x];
+        }
+        System.out.println(sum);
+    }
+}
