adc8235c7ca6bd38bbdaec749560c5bd28b094b3
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-20 19:05:17 +0600

    Add files via upload

---
+++ Tutorials/src/method3.java
@@ -0,0 +1,13 @@
+public class method3 {
+
+    static void sayHello() {
+        System.out.println("Juventus");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
+        sayHello();
+        sayHello();
+        sayHello();
+    }
+}
