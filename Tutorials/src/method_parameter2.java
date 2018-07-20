a16602236691bf82755c373d01239c80f1d581e7
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-20 19:04:38 +0600

    Add files via upload

---
+++ Tutorials/src/method_parameter2.java
@@ -0,0 +1,10 @@
+public class method_parameter2 {
+
+    public static void main(String[] args) {
+        doSomething(4);
+    }
+
+    static void doSomething(int x) {
+        System.out.println(x * x);
+    }
+}
