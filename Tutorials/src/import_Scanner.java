d81f131e8f8f9bc8696df15ac0b4ae4ce970b3c0
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-20 19:02:19 +0600

    Add files via upload

---
+++ Tutorials/src/import_Scanner.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class import_Scanner {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int i;
+        i = num.nextInt();
+        System.out.println("My variable is: " + i);
+    }
+}
