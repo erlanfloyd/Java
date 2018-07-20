5459b09e3cbd73fb02459096f335223cc3bf9fde
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-20 19:04:03 +0600

    Add files via upload

---
+++ Tutorials/src/method_return2.java
@@ -0,0 +1,17 @@
+public class method_return2 {
+
+    // returns an int value 5
+    static int returnFive() {
+        return 5;
+    }
+
+    // has a parameter
+    static void sayHelloTo(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    // simply prints "Hello World!"
+    static void sayHello() {
+        System.out.println("Hello World");
+    }
+}
