9cd27c3e13b068d450bde81e6026f8cadd06a395
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-20 19:03:03 +0600

    Add files via upload

---
+++ Tutorials/src/method2.java
@@ -0,0 +1,10 @@
+public class method2 {
+
+    public static void main(String[] args) {
+        sayHello();
+    }
+
+    static void sayHello() {
+        System.out.println("Hi there");
+    }
+}
