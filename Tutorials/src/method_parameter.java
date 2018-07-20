c1f3c8a49a79d40ecf7ffea1ee5a1424f75dd9eb
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-20 19:04:58 +0600

    Add files via upload

---
+++ Tutorials/src/method_parameter.java
@@ -0,0 +1,11 @@
+public class method_parameter {
+
+    static void sayHello(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    public static void main(String[] args) {
+        sayHello("David");
+        sayHello("Carl");
+    }
+}
