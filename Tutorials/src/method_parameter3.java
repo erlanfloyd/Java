ed224fcc33f9127f55dbe86b08c8f3a3fcde0ac9
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-20 19:03:41 +0600

    Add files via upload

---
+++ Tutorials/src/method_parameter3.java
@@ -0,0 +1,10 @@
+public class method_parameter3 {
+    public static void main(String[] args) {
+        int x = 10;
+        test(x);
+    }
+
+    static void test(int x) {
+        System.out.println(x);
+    }
+}
