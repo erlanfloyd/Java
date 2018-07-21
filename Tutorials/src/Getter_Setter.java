2f9b9c54466996379df70f6d1adcee3d0e6e3ed4
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-21 21:44:04 +0600

    Add files via upload

---
+++ Tutorials/src/Getter_Setter.java
@@ -0,0 +1,15 @@
+public class Getter_Setter {
+    private String color;
+
+    // Getter
+    public String getColor() {
+        return color;
+    }
+
+    // Setter
+    public void setColor(String c) {
+        this.color = c;
+    }
+}
+
+
