706e79516a3b0e42a0dc6b9ca268f44e4e72b295
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-21 21:44:43 +0600

    Add files via upload

---
+++ Tutorials/src/Vehicle.java
@@ -0,0 +1,19 @@
+public class Vehicle {
+    int maxSpeed;
+    int wheels;
+    String color;
+    double fuelCapacity;
+
+    void horn() {
+        System.out.println("Beep!");
+    }
+}
+
+class BMW {
+    public static void main(String[] args) {
+        Vehicle v1 = new Vehicle();
+        Vehicle v2= new Vehicle();
+        v1.color = "red";
+        v2.horn();
+    }
+}
