0456e7d941cd9b32541554653c89d5bb77582977
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-21 21:45:43 +0600

    Add files via upload

---
+++ Tutorials/src/A.java
@@ -0,0 +1,13 @@
+public class A {
+    public void test() {
+        System.out.println("Hi");
+    }
+}
+
+
+class B {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.test();
+    }
+}
