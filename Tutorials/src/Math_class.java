2a0e491d10d8457e896cf362e197f48c35128d8f
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-24 22:02:05 +0600

    Add files via upload

---
+++ Tutorials/src/Math_class.java
@@ -0,0 +1,9 @@
+public class Math_class {
+    public static void main(String[] args) {
+        int a = Math.abs(10);
+        System.out.println(a);
+
+        int b = Math.abs(-20);
+        System.out.println(b);
+    }
+}
