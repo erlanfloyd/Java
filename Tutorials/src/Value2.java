66d77d93e7e187df32838b5078683a6288096bac
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-24 22:02:28 +0600

    Add files via upload

---
+++ Tutorials/src/Value2.java
@@ -0,0 +1,11 @@
+public class Value2 {
+    public static void main(String[] args) {
+        int x = 4;
+        square(x);
+        System.out.println(x);
+    }
+
+    static void square(int x) {
+        x = x * x;
+    }
+}
