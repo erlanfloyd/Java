448df186ad36dcdbb2f394b2ec839c796b6359a7
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-24 22:32:32 +0600

    Add files via upload

---
+++ Tutorials/src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public static int pCount;
+
+    public static void main(String[] args) {
+        Student.pCount = 1;
+        Student.pCount++;
+        System.out.println(Student.pCount);
+    }
+}
