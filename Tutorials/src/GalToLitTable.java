f87053329b10c6d09a0ae9cd2b9d1d9a6a7e3b0d
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-30 11:14:34 +0600

    Add files via upload

---
+++ Tutorials/src/GalToLitTable.java
@@ -0,0 +1,22 @@
+/*
+    Упражнение 1.2
+    Программа отображает таблицу перевода галлонов в литры.
+ */
+public class GalToLitTable {
+    public static void main(String[] args) {
+        double gallons, liters;
+        int counter;
+
+        counter = 0;
+        for (gallons = 1; gallons <= 100; gallons++) {
+            liters = gallons * 3.7854;
+            System.out.println(gallons  + " галлоном соответствует " + liters + " liters. ");
+
+            counter++;
+            if (counter == 10) {
+                System.out.println();
+                counter = 0;
+            }
+        }
+    }
+}
