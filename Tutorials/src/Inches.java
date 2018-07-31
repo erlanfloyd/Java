50358aeabf3f6aeaa2ddf0dba90c7234f3960f03
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-31 12:23:20 +0600

    Add files via upload

---
+++ Tutorials/src/Inches.java
@@ -0,0 +1,15 @@
+/*
+    Расчет числа кубических дюймов в кубе объемом в 1 куб.милю
+ */
+public class Inches {
+    public static void main(String[] args) {
+        long ci;
+        long im;
+
+        im = 5820 * 12;
+
+        ci = im * im * im;
+
+        System.out.println("В одной кубической мили содержится " + ci + " кубических дюймов.");
+    }
+}
