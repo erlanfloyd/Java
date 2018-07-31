07f38536bb6cd712b19cae8ef964ef177d216187
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-07-31 12:22:41 +0600

    Add files via upload

---
+++ Tutorials/src/CharArithDemo.java
@@ -0,0 +1,16 @@
+// С символьными переменными можно обращаться, как с целочисленными
+
+public class CharArithDemo {
+    public static void main(String[] args) {
+        char ch;
+
+        ch = 'X';
+        System.out.println("ch содержит " + ch);
+
+        ch++; // инкрементировать переменную ch
+        System.out.println("теперь ch содержит " + ch);
+
+        ch = 90; // присвоить переменной ch значение z
+        System.out.println("теперь ch содержит " + ch);
+    }
+}
