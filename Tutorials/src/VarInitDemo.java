efbf7a8e33f9f1a7166e9c08e9ec8d4a48769a53
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-08-02 12:20:10 +0600

    Add files via upload

---
+++ Tutorials/src/VarInitDemo.java
@@ -0,0 +1,12 @@
+public class VarInitDemo {
+    public static void main(String[] args) {
+        int x;
+
+        for (x = 0; x < 3; x++) {
+            int y = -1; // переменная "у" инициализируется при каждом входе в блок
+            System.out.println("y: " + y); // всегда выводится значение -1
+            y = 100;
+            System.out.println("Измененное значение у:" + y);
+        }
+    }
+}
