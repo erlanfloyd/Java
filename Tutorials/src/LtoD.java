3bf732bab9ee00345517b0a23a58409a9b267fe4
Yerlan <34455958+erlanfloyd@users.noreply.github.com>
2018-08-03 12:45:27 +0600

    Add files via upload

---
+++ Tutorials/src/LtoD.java
@@ -0,0 +1,10 @@
+public class LtoD {
+    public static void main(String[] args) {
+        long L;
+        double D;
+
+        L = 100123285L;
+        D = L; // Автоматическое преобразование типа long в тип double
+        System.out.println("L и D: " + L + " " + D);
+    }
+}
